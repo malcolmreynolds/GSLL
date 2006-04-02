a367d1ac4a21fa4446510c7e759fd86e052fd4d3
liam <liam@a3d8a0fb-c1db-0310-ace7-a616afeb9e30>
2006-04-02 05:15:11 +0000

    Add copy, swap, and arithmetic functions.  Give up for now on subvector; no port of complex vector functions as this requires a new structure.
    git-svn-id: svn+ssh://pop/opt/space/mathematics/gsl/trunk@3014 a3d8a0fb-c1db-0310-ace7-a616afeb9e30

--- data/vector.lisp
+++ data/vector.lisp
@@ -3,7 +3,7 @@
 ; description: Vectors and matrices                      
 ; date:        Sun Mar 26 2006 - 11:51                   
 ; author:      Liam M. Healy                             
-; modified:    Wed Mar 29 2006 - 23:35
+; modified:    Sat Apr  1 2006 - 23:57
 ;********************************************************
 ;;; $Id: $
 
@@ -12,6 +12,8 @@
 ;;; No mechanism for C stream input/output yet.
 ;;; Generalize check-gsl-status to optionally signal errors, use here?
 ;;; Functions like write-binary etc. as a single function, selecting the C fn with typecase?
+;;; #'subvector, #'subvector-stride cause crash, see notes 2006-03-30
+;;; #'vector-complex-real, #'vector-complex-imag need structure definition
 
 ;;; Syntax:
 ;;; (setf (gsl-aref vec 0) -3.21d0)
@@ -140,17 +142,129 @@ deallocated with the vector.
 from the start of the original vector.  The new vector has @var{size}
 elements.")
 
+(defun-gsl subvector-stride
+    ((vector gsl-vector-c) (offset :size) (stride :size) (size :size))
+  "gsl_vector_subvector_with_stride"
+  :c-return-value :return
+  :return (gsl-vector-view)
+  :documentation
+  "")
+
+;;; These require that the gsl-vector-complex structure be defined.
+#|
+(defun-gsl vector-complex-real ((vector gsl-vector-complex))
+  "gsl_vector_complex_real"
+  :c-return-value :return
+  :return (gsl-vector-view)
+  :documentation
+  "A vector view of the real parts of the complex vector @var{v}.")
+
+(defun-gsl vector-complex-imag ((vector gsl-vector-complex))
+  "gsl_vector_complex_imag"
+  :c-return-value :return
+  :return (gsl-vector-view)
+  :documentation
+  "A vector view of the imaginary parts of the complex vector @var{v}.")
+|#
+
+(defun-gsl view-array ((base :pointer) (size :size))
+  "gsl_vector_view_array"
+  :c-return-value :return
+  :return (gsl-vector-view)
+  :documentation
+  "A vector view of an array.  The start of the new
+vector is given by @var{base} and has @var{n} elements.")
+
+(defun-gsl view-array-stride ((base :pointer) (stride :size) (size :size))
+  "gsl_vector_view_array_with_stride"
+  :c-return-value :return
+  :return (gsl-vector-view)
+  :documentation
+  "A vector view of an array with stride.  The start of the new
+vector is given by @var{base}.")
+
 ;;;;****************************************************************************
 ;;;; Copying
 ;;;;****************************************************************************
+
+(defun-gsl vector-copy
+    ((destination gsl-vector-c) (source gsl-vector-c) )
+  "gsl_vector_memcpy"
+  :documentation
+  "Copy the elements of the vector @var{src} into the
+   vector @var{dest}.  The two vectors must have the same length.")
+
+(defun-gsl vector-swap
+    ((v gsl-vector-c) (w gsl-vector-c) )
+  "gsl_vector_swap"
+  :documentation
+  "Exchanges the elements of the vectors @var{v} and @var{w}
+   by copying.  The two vectors must have the same length.")
 
 ;;;;****************************************************************************
 ;;;; Exchanging elements
 ;;;;****************************************************************************
 
+(defun-gsl vector-swap ((vec gsl-vector-c) (i :size) (j :size))
+  "gsl_vector_swap_elements"
+  :documentation
+  "Exchange the @var{i}-th and @var{j}-th elements of the
+   vector @var{v} in-place.")
+
+(defun-gsl vector-reverse ((vec gsl-vector-c))
+  "gsl_vector_reverse"
+  :documentation
+  "Reverse the order of the elements of the vector @var{v}.")
+
 ;;;;****************************************************************************
 ;;;; Arithmetic operations
 ;;;;****************************************************************************
+
+(defun-gsl vector+
+    ((a gsl-vector-c) (b gsl-vector-c) )
+  "gsl_vector_add"
+  :documentation
+  "Add the elements of vector @var{b} to the elements of
+vector @var{a}, @math{a'_i = a_i + b_i}. The two vectors must have the
+same length.")
+
+(defun-gsl vector-
+    ((a gsl-vector-c) (b gsl-vector-c) )
+  "gsl_vector_sub"
+  :documentation
+  "Subtract the elements of vector @var{b} from the elements of
+vector @var{a}, @math{a'_i = a_i - b_i}. The two vectors must have the
+same length.")
+
+(defun-gsl vector*
+    ((a gsl-vector-c) (b gsl-vector-c) )
+  "gsl_vector_mul"
+  :documentation
+  "Multiply the elements of vector @var{a} by the elements of
+vector @var{b}, @math{a'_i = a_i * b_i}. The two vectors must have the
+same length.")
+
+(defun-gsl vector/
+    ((a gsl-vector-c) (b gsl-vector-c) )
+  "gsl_vector_div"
+  :documentation
+  "Divide the elements of vector @var{a} by the elements of
+vector @var{b}, @math{a'_i = a_i / b_i}. The two vectors must have the
+same length.")
+
+(defun-gsl vector*c
+    ((a gsl-vector-c) (x :double))
+  "gsl_vector_scale"
+  :documentation
+  "Multiply the elements of vector @var{a} by the constant
+factor @var{x}, @math{a'_i = x a_i}.")
+
+(defun-gsl vector+c
+    ((a gsl-vector-c) (x :double))
+  "gsl_vector_add_constant"
+  :documentation
+  "Add the constant value @var{x} to the elements of the
+vector @var{a}, @math{a'_i = a_i + x}.")
 
 ;;;;****************************************************************************
 ;;;; Maximum and minimum elements
@@ -165,23 +279,26 @@ elements.")
 ;;;;****************************************************************************
 
 #|
+(defun print-vector (vec)
+  (format t "~&~f ~f ~f"
+	  (gsl-aref vec 0) (gsl-aref vec 1) (gsl-aref vec 2)))
+
+(defun set-vector (vec &rest values)
+  (setf (gsl-aref vec 0) (nth 0 values)
+	(gsl-aref vec 1) (nth 1 values)
+	(gsl-aref vec 2) (nth 2 values)))
+
 (with-data (vec vector 3)
   (setf (gsl-aref vec 0) -3.21d0
 	(gsl-aref vec 1) 1.0d0
 	(gsl-aref vec 2) 12.8d0)
-  (print (gsl-aref vec 2))
-  (print (gsl-aref vec 1))
-  (print (gsl-aref vec 0)))
+  (print-vector vec))
 
 (with-data (vec vector 3)
   (set-all vec 77.8d0)
-  (print (gsl-aref vec 2))
-  (print (gsl-aref vec 1))
-  (print (gsl-aref vec 0))
+  (print-vector vec)
   (set-zero vec)
-  (print (gsl-aref vec 2))
-  (print (gsl-aref vec 1))
-  (print (gsl-aref vec 0)))
+  (print-vector vec))
 
 (with-data (vec vector 3) (set-basis vec 1)
 	   (format t "~&~a ~a ~a"
@@ -217,5 +334,28 @@ elements.")
 			  2
 			  GSL-VECTOR-VIEW))
 
-|#
+(with-data (vec1 vector 3)
+  (with-data (vec2 vector 3)
+    (setf (gsl-aref vec1 0) -3.21d0
+	  (gsl-aref vec1 1) 1.0d0
+	  (gsl-aref vec1 2) 12.8d0)
+    (vector-copy vec2 vec1)
+    (print-vector vec2)))
 
+(with-data (vec1 vector 3)
+  (set-vector vec1 -3.21d0 1.0d0 12.8d0) (print-vector vec1)
+  (vector-swap vec1 0 1) (print-vector vec1)
+  (vector-reverse vec1) (print-vector vec1)
+  (vector*c vec1 2.0d0) (print-vector vec1)
+  (vector+c vec1 2.0d0) (print-vector vec1))
+
+(with-data (vec1 vector 3)
+  (with-data (vec2 vector 3)
+    (set-vector vec1 -3.21d0 1.0d0 12.8d0)
+    (set-vector vec2 1.4d0 17.0d0 -3.0d0)
+    (vector+ vec1 vec2)
+    (print-vector vec1)
+    (vector* vec1 vec2)
+    (print-vector vec1)))
+
+|#
