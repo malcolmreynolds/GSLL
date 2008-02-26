919f01f2021250c74e84ff83bf05a178ea241fec
lhealy <lhealy@af03a46b-e846-0410-96e5-d5653c316fd0>
2008-02-26 00:32:39 +0000

    Add Mac OSX paths to libgsl and libcblas.
    git-svn-id: svn+ssh://common-lisp.net/project/gsll/svn/trunk@25 af03a46b-e846-0410-96e5-d5653c316fd0

--- init/init.lisp
+++ init/init.lisp
@@ -1,6 +1,6 @@
 ;; Load GSL
 ;; Liam Healy Sat Mar  4 2006 - 18:53
-;; Time-stamp: <2008-02-17 18:58:32EST init.lisp>
+;; Time-stamp: <2008-02-25 19:31:49EST init.lisp>
 ;; $Id: $
 
 (defpackage gsll
@@ -8,12 +8,14 @@
   (:use :common-lisp :cffi))
 
 (cffi:define-foreign-library libgslcblas
+  (:darwin (:or "/opt/local/lib/libgslcblas.dylib" "/usr/local/lib/libgslcblas.dylib"))
   (:unix (:or "/usr/lib/libgslcblas.so.0" "/usr/lib/libgslcblas.so"))
   (t (:default "libgslcblas")))
    
 (cffi:use-foreign-library libgslcblas)
 
 (cffi:define-foreign-library libgsl
+  (:darwin (:or "/opt/local/lib/libgsl.dylib" "/usr/local/lib/libgsl.dylib"))
   (:unix (:or "/usr/lib/libgsl.so.0" "/usr/lib/libgsl.so"))
   (t (:default "libgsl")))
    
