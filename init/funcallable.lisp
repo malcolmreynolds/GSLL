196f0d4eb371ba07e1f5160c131e798eea443b1f
Liam Healy <liam@thinkpad.local>
2009-03-28 18:14:26 -0400

    Complete make-funcallable
    The function #'make-funcallable creates a lambda form that acts as a
    bridge between the callback as passed to GSL and the user's function.
    It can be optionally compiled and is funcalled in the defmcallback
    expansion; it takes the user's function as an argument.

--- init/funcallable.lisp
+++ init/funcallable.lisp
@@ -1,96 +1,156 @@
-;; Generate a wrapper to be called by callback, that calls user function.
+;; Generate a lambda that calls the user function; will be called by callback.
 ;; Liam Healy 
-;; Time-stamp: <2009-03-26 23:19:47EDT funcallable.lisp>
+;; Time-stamp: <2009-03-28 18:11:09EDT funcallable.lisp>
 ;; $Id$
 
 (in-package :gsl)
 
+;;;;****************************************************************************
+;;;; Utility 
+;;;;****************************************************************************
+
 (defun make-symbol-cardinal (name i)
   (make-symbol (format nil "~:@(~a~)~d" name i)))
 
-(defun reference-foreign-element (foreign-variable-name index argspec)
+(defun make-symbol-cardinals (name max-count)
+  (loop for i from 0 below max-count
+       collect (make-symbol-cardinal name i)))
+
+(defun value-from-dimensions (argspec dimension-values &optional total)
+  "Return a list of numerical sizes for dimensions of an array.  If
+   total = T, then return the product of those dimensions."
+  ;; (DIM0) -> numerical value
+  ;; (DIM0 DIM0) -> product of numerical values
+  (let ((list
+	 (subst (first dimension-values)
+		'dim0
+		(subst (second dimension-values)
+		       'dim1
+		       (parse-callback-argspec argspec 'dimensions)))))
+    (if total
+	(apply '* list) list)))
+
+(defun all-io (direction &optional (arrays t))
+  "Create a function that returns dimensions for argspecs that are arrays
+   for the specified direction."
+  (if arrays
+      (lambda (arg)
+	(and (listp arg)
+	     (eql (parse-callback-argspec arg 'io) direction)
+	     (parse-callback-argspec arg 'dimensions)))
+      (lambda (arg)
+	(and (listp arg)
+	     (eql (parse-callback-argspec arg 'io) direction)))))
+
+;;;;****************************************************************************
+;;;; Reference foreign elements and make multiple-value-bind form
+;;;;****************************************************************************
+
+(defun reference-foreign-element
+    (foreign-variable-name linear-index argspec dimension-values)
   "Form to reference, for getting or setting, the element of a foreign
    array, or a scalar."
   (if (parse-callback-argspec argspec 'dimensions)
-      (if (eql (parse-callback-argspec argspec 'dimensions) :marray)
+      (if (eql (parse-callback-argspec argspec 'array-type) :marray)
 	  `(maref
 	    ,foreign-variable-name
-	    ;; need to pad NIL for vectors
-	    ,@(parse-callback-argspec argspec 'dimensions)
+	    ,@(let ((dims (value-from-dimensions argspec dimension-values)))
+		   (if (= (length dims) 2) ; matrix
+		       (multiple-value-list
+			(floor linear-index
+			       (first dims)))
+		       (list linear-index nil)))
 	    ',(cffi-cl (parse-callback-argspec argspec 'element-type)))
 	  `(cffi:mem-aref
 	    ,foreign-variable-name
 	    ',(parse-callback-argspec argspec 'element-type)
-	    ,index))
+	    ,linear-index))
       ;; not setfable if it's a scalar
       foreign-variable-name))
 
-(defun callback-set-mvb (argument-names form fnspec)
+(defun array-element-refs (array-names argspecs dimension-values)
+  "A list of forms reference each array element in succession."
+  (loop for arg in argspecs
+     for count = (value-from-dimensions arg dimension-values t)
+     for name in array-names
+     append
+     (loop for i from 0 below count
+	collect (reference-foreign-element name i arg dimension-values))))
+
+(defun callback-set-mvb (argument-names form fnspec dimension-values)
   "Create the multiple-value-bind form in the callback to set the return C arrays."
   (let* ((setargs		 ; arguments that are arrays being set
 	  (remove-if-not
-	   (lambda (arg)
-	     (and (eql (parse-callback-argspec arg 'io) :output)
-		  (parse-callback-argspec arg 'dimensions)))
+	   (all-io :output)
 	   (parse-callback-fnspec fnspec 'arguments-spec)))
 	 (counts		  ; number of scalars for each set arg
-	  (mapcar
-	   (lambda (arg)
-	     (abs (apply '* (parse-callback-argspec arg 'dimensions))))
-	   setargs))
+	  (mapcar (lambda (arg) (value-from-dimensions arg dimension-values t))
+		  setargs))
 	 (count				; total number of scalars set
 	  (apply '+ counts))
 	 (mvbvbls	      ; the symbols to be multiple-value-bound
 	  (loop for i from 0 below count
 	     collect (make-symbol-cardinal 'setscalar i)))
-	 (setvbls
-	  (loop for arg in setargs
-	     for count in counts
-	     append
-	     (loop for i from 0 below count
-		collect (reference-foreign-element argument-names i arg)))))
+	 (setvbls (array-element-refs argument-names setargs dimension-values)))
+    ;;(lu:print-variables setargs counts count mvbvbls setvbls)
     (if (zerop count)
 	form
 	`(multiple-value-bind ,mvbvbls
 	     ,form
-	   (setf ,@(loop for mvbvbl in mvbvbls
-		      for setvbl in setvbls
-		      append (list setvbl mvbvbl)))))))
+	   (setf ,@(loop for arg in setargs
+		      for mvbvbl in mvbvbls
+		      for count in counts
+		      with svs = (copy-list setvbls)
+		      and mvv = (copy-list mvbvbls)
+		      append
+		      (loop for i from 0 below count
+			 append (list (pop svs) (pop mvv)))))))))
 
-(defun make-funcallable (dynfn-variable fnspec)
-  "Define a wrapper function to interface GSL with the user's function"
+;;;;****************************************************************************
+;;;; Create a lambda form suitable for call by defmcallback
+;;;;****************************************************************************
+
+(defun make-funcallable (user-function fnspec scalarsp dimension-values)
+  "Define a wrapper function to interface GSL with the user's function.
+   scalarsp will be either T or NIL, depending on whether the user function
+   expects and returns scalars, and dimension-values should be a list
+   of number(s), (dim0) or (dim0 dim1), or NIL."
   (let* ((argspecs (parse-callback-fnspec fnspec 'arguments-spec))
-	 (noslug (remove :slug argspecs))
 	 (paramsarg (make-symbol "PARAMS"))
+	 (inargs-specs (remove nil (mapcar (all-io :input) argspecs)))
+	 (inargs-names
+	  (make-symbol-cardinals
+	   'input
+	   (length (remove nil (mapcar (all-io :input nil) argspecs)))))
+	 (outargs-specs (remove nil (mapcar (all-io :output) argspecs)))
+	 (outargs-names (make-symbol-cardinals 'output (length outargs-specs)))
 	 (lambda-args
 	  (loop for arg in argspecs
-	     with count = 0
-	     collect
+	     with oarg = (copy-list outargs-names)
+	     and iarg = (copy-list inargs-names)
+	     append
 	     (if (eql arg :slug)
-		 paramsarg
+		 (list paramsarg)
 		 (progn
-		   (if (eql (parse-callback-argspec arg 'io) :output)
-		       (make-symbol-cardinal 'output count)
-		       (make-symbol-cardinal 'input count))
-		   (incf count)))))
-	 (call-form
-	  `(funcall
-	    (first ,dynfn-variable)
-	    ,@(loop ;; over all elements of input variables
-		 (reference-foreign-element ???????)))))
-    `(lambda
-	 (,lambda-args)
+		   (if (and (eql (parse-callback-argspec arg 'io) :output)
+			    (parse-callback-argspec arg 'array-type))
+		       (list (pop oarg))
+		       (if (eql (parse-callback-argspec arg 'io) :input)
+			   (list (pop iarg)))))))))
+    `(lambda ,lambda-args
        ;; Parameters as C argument are always ignored, because we have
        ;; CL specials to do the same job.
-       (declare (ignore ,paramsarg) (special ,dynfn-variable))
-       ,(if (find :output argspecs
-		  :key (lambda (arg) (parse-callback-argspec arg 'io)))
-	    ,(callback-set-mvb
-	      argument-names
-	      call-form
-	      fnspec)
-	    ;; no specified output, return what the function returns
-	    call-form)
+       (declare (ignore ,paramsarg))
+       ,(if (and scalarsp (or inargs-specs outargs-specs))
+	    (let ((call-form
+		   `(funcall
+		     ,user-function
+		     ,@(array-element-refs inargs-names inargs-specs dimension-values))))
+	      (if outargs-specs
+		  (callback-set-mvb outargs-names call-form fnspec dimension-values)
+		  ;; no specified output, return what the function returns
+		  call-form))
+	    `(funcall ,user-function ,@(append inargs-names outargs-names)))
        ,@(case
 	  (parse-callback-fnspec fnspec 'return-spec)
 	  (:success-failure
