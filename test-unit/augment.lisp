e0df4389b772f9b584dac15e74e42d1ffd078df9
Liam Healy <lnp@healy.washington.dc.us>
2009-04-30 12:29:25 -0400

    Fix lisp-unit:numerical-equal definition
    Method lisp-unit:numerical-equal on marrays always returned T; now
    fixed.

--- test-unit/augment.lisp
+++ test-unit/augment.lisp
@@ -1,13 +1,13 @@
 ;; Additional methods for lisp-unit
 ;; Liam Healy 2009-04-15 23:23:30EDT augment.lisp
-;; Time-stamp: <2009-04-15 23:24:22EDT augment.lisp>
+;; Time-stamp: <2009-04-30 12:25:37EDT augment.lisp>
 ;; $Id: $
 
 (in-package :gsl)
 
 (defmethod lisp-unit:numerical-equal
-   ((result1 marray) (result2 marray) &key (test #'lisp-unit:number-equal))
- "Return true if the arrays are numerically equal according to :TEST."
- (when (equal (dimensions result1) (dimensions result2))
-   (lisp-unit:numerical-equal (cl-array result1) (cl-array result2))
-   :test test))
+    ((result1 marray) (result2 marray) &key (test #'lisp-unit:number-equal))
+  "Return true if the arrays are numerically equal according to :TEST."
+  (when (equal (dimensions result1) (dimensions result2))
+    (lisp-unit:numerical-equal (cl-array result1) (cl-array result2)
+			       :test test)))
