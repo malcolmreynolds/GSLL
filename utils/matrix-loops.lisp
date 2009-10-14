139e42dee3447b26ee92845661cee4fcd5160b7f
Malcolm Reynolds <malcolm.reynolds@gmail.com>
2009-10-14 18:08:55 +0100

    Added symbol-macrolets to most of the do-matrix-* macros.
    This means the symbol 'elm' can be used within the body given
    to these macros and it will expand into a (maref matrix i j) and
    so can be used to get or setf that element. I am yet to figure out
    how to add a real macro to do-matrix-[cols,rows] which will take
    one argument, so that
    
    (do-matrix-rows (mtx r)
      (princ (elm 1)))
    
    will print the second element of each row. There has to be a way...

--- utils/matrix-loops.lisp
+++ utils/matrix-loops.lisp
@@ -12,44 +12,54 @@
 
 (defmacro do-vector ((vector i) &body body)
   "Executes ,@body once for each element of vector, with i
-   bound to the index of this element. use-assert argument controls
-   whether to include an assertion the vector really is a gsll vector."
+   bound to the index of this element. 
+
+   In the body 'elm' is a symbol macro expanding to (maref vector i), ie
+   it is used to get or set the current element."
   (let ((m (gensym)))
-    `(let ((,m (dim0 ,vector)))
-       (do ((,i 0 (1+ ,i)))
-	   ((= ,i ,m))
-	 (declare (fixnum ,i))
-	 ,@body))))
+    `(symbol-macrolet ((elm (maref ,vector ,i)))
+       (let ((,m (dim0 ,vector)))
+	 (do ((,i 0 (1+ ,i)))
+	     ((= ,i ,m))
+	   (declare (fixnum ,i))
+	   ,@body)))))
 
 (defmacro do-matrix ((matrix i j) &body body)
   "Executes ,@body once for each element of matrix, with i and
-   and j bound to the row and column indices respectively."
+   and j bound to the row and column indices respectively.
+
+   In the body 'elm' is a symbol macro expanding to (maref matrix i j),
+   ie it is used to get or set the current element."
   (let ((m (gensym))
 	(n (gensym)))
-    `(let ((,m (dim0 ,matrix))
-	   (,n (dim1 ,matrix)))
-       (do ((,i 0 (1+ ,i)))
-	   ((= ,i ,m))
-	 (declare (fixnum ,i))
-	 (do ((,j 0 (1+ ,j)))
-	     ((= ,j ,n))
-	   (declare (fixnum ,j))
-	   ,@body)))))
+    `(symbol-macrolet ((elm (maref ,matrix ,i ,j)))
+       (let ((,m (dim0 ,matrix))
+	     (,n (dim1 ,matrix)))
+	 (do ((,i 0 (1+ ,i)))
+	     ((= ,i ,m))
+	   (declare (fixnum ,i))
+	   (do ((,j 0 (1+ ,j)))
+	       ((= ,j ,n))
+	     (declare (fixnum ,j))
+	     ,@body))))))
 
 (defmacro do-matrix-up-triangular ((matrix i j) &body body)
   "Executes ,@body once for each element of the upper triangular
    portion of the matrix, with i and j bound to the row and column
-   indices respectively. Includes the major diagonal."
+   indices respectively. Includes the major diagonal.
+
+   elm is a symbol-macro to access current element, see do-matrix..."
   (let ((m (gensym)) (n (gensym)))
-    `(let ((,m (dim0 ,matrix))
-	   (,n (dim1 ,matrix)))
-       (do ((,i 0 (1+ ,i)))
-	   ((= ,i ,m))
-	 (declare (fixnum ,i))
-	 (do ((,j ,i (1+ ,j)))
-	     ((= ,j ,n))
-	   (declare (fixnum ,j))
-	   ,@body)))))
+    `(symbol-macrolet ((elm (maref ,matrix ,i ,j)))
+       (let ((,m (dim0 ,matrix))
+	     (,n (dim1 ,matrix)))
+	 (do ((,i 0 (1+ ,i)))
+	     ((= ,i ,m))
+	   (declare (fixnum ,i))
+	   (do ((,j ,i (1+ ,j)))
+	       ((= ,j ,n))
+	     (declare (fixnum ,j))
+	     ,@body))))))
 
 (defmacro do-matrix-rows ((matrix r) &body body)
   "Executes ,@body once for each row of the matrix, with r bound
@@ -74,8 +84,9 @@
 (defmacro do-matrix-diag ((matrix i) &body body)
   "Executes ,@body once for every diagonal element of the matrix."
   (let ((m (gensym)))
-    `(let ((,m (min (dim0 ,matrix) (dim1 ,matrix))))
-       (do ((,i 0 (1+ ,i)))
-	   ((= ,i ,m))
-	 (declare (fixnum ,i))
-	 ,@body))))
+    `(symbol-macrolet ((elm (maref ,matrix ,i ,i)))
+       (let ((,m (min (dim0 ,matrix) (dim1 ,matrix))))
+	 (do ((,i 0 (1+ ,i)))
+	     ((= ,i ,m))
+	   (declare (fixnum ,i))
+	   ,@body)))))
