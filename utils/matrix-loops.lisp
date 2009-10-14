93e7f0d063d8af12891b41f18c5bd0c822165182
Malcolm Reynolds <malcolm.reynolds@gmail.com>
2009-10-15 00:19:34 +0100

    Fixed bug with the symbol macro being only in the gsll-util package.
    Now if people want a symbol macro like 'elm' instead of '(maref m i j)'
    they will have to provide a symbol for it to be bound to. This argument
    to all the macros is optional, if they don't want to provide it then
    no symbol macro is created.

--- utils/matrix-loops.lisp
+++ utils/matrix-loops.lisp
@@ -10,100 +10,104 @@
 ;; to automatically chuck in an (typep v 'gsll:mvector) form as
 ;; appropriate
 
-(defmacro do-vector ((vector i) &body body)
-  "Executes ,@body once for each element of vector, with i
-   bound to the index of this element. 
+(defmacro do-vector ((vector i &optional elm) &body body)
+  "Executes ,@body once for each element of vector, with i bound to
+   the index of this element.
 
-   In the body 'elm' is a symbol macro expanding to (maref vector i), ie
-   it is used to get or set the current element."
+   If provided, elm becomes a symbol macro to get/setf the
+   current element inside the body."
   (let ((m (gensym)))
-    `(symbol-macrolet ((elm (maref ,vector ,i)))
-       (let ((,m (dim0 ,vector)))
-	 (declare (fixnum ,m))
-	 (do ((,i 0 (1+ ,i)))
-	     ((= ,i ,m))
-	   (declare (fixnum ,i))
-	   ,@body)))))
+    (let ((inner-code `(let ((,m (dim0 ,vector)))
+			 (declare (fixnum ,m))
+			 (dotimes (,i ,m)
+			   ,@body))))
+      (if elm
+	  `(symbol-macrolet ((,elm (maref ,vector ,i)))
+	     ,inner-code)
+	  inner-code))))
 
-(defmacro do-matrix ((matrix i j) &body body)
-  "Executes ,@body once for each element of matrix, with i and
-   and j bound to the row and column indices respectively.
+(defmacro do-matrix ((matrix i j &optional elm) &body body)
+  "Executes ,@body once for each element of matrix, with i and and j
+   bound to the row and column indices respectively.
 
-   In the body 'elm' is a symbol macro expanding to (maref matrix i j),
-   ie it is used to get or set the current element."
-  (let ((m (gensym))
-	(n (gensym)))
-    `(symbol-macrolet ((elm (maref ,matrix ,i ,j)))
-       (let ((,m (dim0 ,matrix))
-	     (,n (dim1 ,matrix)))
-	 (declare (fixnum ,m ,n))
-	 (do ((,i 0 (1+ ,i)))
-	     ((= ,i ,m))
-	   (declare (fixnum ,i))
-	   (do ((,j 0 (1+ ,j)))
-	       ((= ,j ,n))
-	     (declare (fixnum ,j))
-	     ,@body))))))
+   If provided, elm becomes a symbol macro to get/setf the
+   current element inside the body."
+  (let ((m (gensym)) (n (gensym)))
+    (let ((inner-code `(let ((,m (dim0 ,matrix))
+			     (,n (dim1 ,matrix)))
+			 (declare (fixnum ,m ,n))
+			 (dotimes (,i ,m)
+			   (dotimes (,j ,n)
+			     ,@body)))))
+      (if elm
+	  `(symbol-macrolet ((,elm (maref ,matrix ,i ,j)))
+	     ,inner-code)
+	  inner-code))))
 
-(defmacro do-matrix-up-triangular ((matrix i j) &body body)
+(defmacro do-matrix-up-triangular ((matrix i j &optional elm) &body body)
   "Executes ,@body once for each element of the upper triangular
    portion of the matrix, with i and j bound to the row and column
    indices respectively. Includes the major diagonal.
 
-   elm is a symbol-macro to access current element, see do-matrix..."
+   If provided, elm becomes a symbol macro to get/setf the
+   current element inside the body."
   (let ((m (gensym)) (n (gensym)))
-    `(symbol-macrolet ((elm (maref ,matrix ,i ,j)))
-       (let ((,m (dim0 ,matrix))
-	     (,n (dim1 ,matrix)))
-	 (declare (fixnum ,m ,n))
-	 (do ((,i 0 (1+ ,i)))
-	     ((= ,i ,m))
-	   (declare (fixnum ,i))
-	   (do ((,j ,i (1+ ,j)))
-	       ((= ,j ,n))
-	     (declare (fixnum ,j))
-	     ,@body))))))
+    (let ((inner-code `(let ((,m (dim0 ,matrix))
+			     (,n (dim1 ,matrix)))
+			 (declare (fixnum ,m ,n))
+			 (dotimes (,i ,m)
+			   (do ((,j ,i (1+  ,j)))
+			       ((= ,j ,n))
+			     (declare (fixnum ,j))
+			     ,@body)))))
+      (if elm
+	  `(symbol-macrolet ((,elm (maref ,matrix ,i ,j)))
+	     ,inner-code)
+	  inner-code))))
 
-(defmacro do-matrix-rows ((matrix r) &body body)
-  "Executes ,@body once for each row of the matrix, with r bound
-   the the row index.
+(defmacro do-matrix-rows ((matrix r &optional row-name) &body body)
+  "Executes ,@body once for each row of the matrix, with r bound the
+   the row index.
 
-   Inside the body row is a symbol-macro which can be used to get
-   or set the current row."
+   If provided, row-name becomes a symbol macro to get/setf the
+   current row inside the body."
   (let ((m (gensym)))
-    `(symbol-macrolet ((row (row ,matrix ,r)))
-       (let ((,m (dim0 ,matrix)))
-	 (declare (fixnum ,m))
-	 (do ((,r 0 (1+ ,r)))
-	     ((= ,r ,m))
-	   (declare (fixnum ,r))
-	   ,@body)))))
+    (let ((inner-code `(let ((,m (dim0 ,matrix)))
+			 (declare (fixnum ,m))
+			 (dotimes (,r ,m)
+			   ,@body))))
+      (if row-name
+	  `(symbol-macrolet ((,row-name (row ,matrix ,r)))
+	     ,inner-code)
+	  inner-code))))
 
-(defmacro do-matrix-cols ((matrix c) &body body)
-  "Executes ,@body once for each column of the matrix, with c bound
-   to the column index.
+(defmacro do-matrix-cols ((matrix c &optional column-name) &body body)
+  "Executes ,@body once for each column of the matrix, with c bound to
+   the column index.
 
-   Inside the body col is a symbol-macro which can be used to get
-   or set the current column."
+   If provided, column-name becomes a symbol-macro to get/setf the
+   current column inside the body."
   (let ((m (gensym)))
-    `(symbol-macrolet ((col (column ,matrix ,c)))
-       (let ((,m (dim1 ,matrix)))
-	 (declare (fixnum ,m))
-	 (do ((,c 0 (1+ ,c)))
-	     ((= ,c ,m))
-	   (declare (fixnum ,c))
-	   ,@body)))))
+    (let ((inner-code `(let ((,m (dim1 ,matrix)))
+			 (declare (fixnum ,m))
+			 (dotimes (,c ,m)
+			   ,@body))))
+      (if column-name
+	  `(symbol-macrolet ((,column-name (column ,matrix ,c)))
+	     ,inner-code)
+	  inner-code))))
 
-(defmacro do-matrix-diag ((matrix i) &body body)
+(defmacro do-matrix-diag ((matrix i &optional elm) &body body)
   "Executes ,@body once for every diagonal element of the matrix.
 
-   Inside the body elm is a symbol-macro which can be used to get
-   or set the current-column."
+   If provided, elm becomes a symbol macro to get/setf the current
+   element inside the body."
   (let ((m (gensym)))
-    `(symbol-macrolet ((elm (maref ,matrix ,i ,i)))
-       (let ((,m (min (dim0 ,matrix) (dim1 ,matrix))))
-	 (declare (fixnum ,m))
-	 (do ((,i 0 (1+ ,i)))
-	     ((= ,i ,m))
-	   (declare (fixnum ,i))
-	   ,@body)))))
+    (let ((inner-code `(let ((,m (min (dim0 ,matrix) (dim1 ,matrix))))
+			 (declare (fixnum ,m))
+			 (dotimes (,i ,m)
+			   ,@body))))
+      (if elm
+	  `(symbol-macrolet ((,elm (maref ,matrix ,i ,i)))
+	     ,inner-code)
+	  inner-code))))
